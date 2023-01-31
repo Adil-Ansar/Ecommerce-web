ca1e4c81b86bed3c9b2eee9d92f89c216f4c5146
Adil <ansari.adil2015@gmail.com>
2023-01-31 22:36:46 +0530

    Added createProductReview API

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,19 +1,26 @@
 const express = require("express");
 const { isAuthenticatedUSer, authorizeRoles } = require("../middleware/auth");
-const { getAllProduct, createProduct, updateProduct, deleteProduct, getProductDetails } = require("../controller/productController");
+const { getAllProduct,
+    createProduct,
+    updateProduct,
+    deleteProduct,
+    getProductDetails,
+    createProductReview } = require("../controller/productController");
 
 
 const router = express.Router();
 
 router.get("/product", getAllProduct);
 
-router.post("/admin/product/new", isAuthenticatedUSer, authorizeRoles("admin"), createProduct);``
+router.post("/admin/product/new", isAuthenticatedUSer, authorizeRoles("admin"), createProduct); ``
 
 router.put("/admin/product/:id", isAuthenticatedUSer, authorizeRoles("admin"), updateProduct);
 
 router.delete("/admin/product/:id", isAuthenticatedUSer, authorizeRoles("admin"), deleteProduct);
 
 router.get("/product/:id", getProductDetails);
+
+router.put("/review", isAuthenticatedUSer, createProductReview);
 
 
 module.exports = router;
